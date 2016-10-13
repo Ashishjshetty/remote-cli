c6ce788d9193e32fabfdfca4ac8139337f6819e3
Ashish shetty <ashishshetty2007@gmail.com>
2016-10-13 13:36:09 +0530

    first commit

--- routes/index.js
+++ routes/index.js
@@ -6,17 +6,17 @@ var router = express.Router();
 
 
 router.get('/', function(req, res) {
-	var server_list = _.pluck(process.rooms.server, 'id');
-	console.log(process.rooms.server);
-	res.render('home', {
-		server_list: server_list
-	});
+    var server_list = _.pluck(process.rooms.server, 'id');
+    console.log(process.rooms.server);
+    res.render('home', {
+        server_list: server_list
+    });
 });
 router.get('/connect/:id', function(req, res) {
-	var id = req.params.id;
-	res.render('index', {
-		url: 'ws://127.0.0.1:8080',
-		server: id
-	});
+    var id = req.params.id;
+    res.render('index', {
+        url: 'ws://127.0.0.1:8080',
+        server: id
+    });
 });
 module.exports = router;
--- routes/socket.js
+++ routes/socket.js
@@ -3,98 +3,98 @@ var _ = require('underscore');
 
 var io = process.io;
 process.rooms = {
-  server: [],
-  browser: []
+    server: [],
+    browser: []
 };
 
 io.sockets.on('connection', function(sock) {
-  // console.log(sock.handshake.query);
+    console.log(sock.request.headers);
 
-  if (sock.request.headers['user-agent'] === 'node-XMLHttpRequest') {
-    sock.join('server');
-    sock.room = 'server';
-    sock.connId = sock.handshake.query.id;
-    process.rooms.server.push({
-      "id": sock.connId,
-      socket: sock
-    });
-    // console.log(rooms.browser);
-  } else {
-    sock.join('browser');
-    sock.room = 'browser';
-    sock.connId = sock.handshake.query.id;
-    process.rooms.browser.push({
-      "id": sock.connId,
-      socket: sock,
-      connect: sock.handshake.query.connectTo
-    });
+    if (sock.request.headers['user-agent'] === 'node-XMLHttpRequest') {
+        sock.join('server');
+        sock.room = 'server';
+        sock.connId = sock.handshake.query.id;
+        process.rooms.server.push({
+            "id": sock.connId,
+            socket: sock
+        });
+        // console.log(rooms.browser);
+    } else {
+        sock.join('browser');
+        sock.room = 'browser';
+        sock.connId = sock.handshake.query.id;
+        process.rooms.browser.push({
+            "id": sock.connId,
+            socket: sock,
+            connect: sock.handshake.query.connectTo
+        });
 
-    for (var index in process.rooms.server) {
+        for (var index in process.rooms.server) {
 
-      if (process.rooms.server[index].id === sock.handshake.query.connectTo) {
+            if (process.rooms.server[index].id === sock.handshake.query.connectTo) {
 
-        process.rooms.server[index].connect = sock.connId;
+                process.rooms.server[index].connect = sock.connId;
 
-      }
+            }
+        }
+        // console.log(rooms.browser);
     }
-    // console.log(rooms.browser);
-  }
-  console.log('connected:', process.rooms.server.length, process.rooms.browser.length, sock.id);
-  var socketInfo = new socketsInfo(sock);
+    console.log('connected:', process.rooms.server.length, process.rooms.browser.length, sock.id);
+    var socketInfo = new socketsInfo(sock);
 
-  socketInfo.onData(function brodcast(data, socket) {
-    console.log(data);
+    socketInfo.onData(function brodcast(data, socket) {
+        console.log(data);
 
-    var toRoom = (socket.room === 'server') ? 'browser' : 'server';
+        var toRoom = (socket.room === 'server') ? 'browser' : 'server';
 
-    var toID = _.findWhere(process.rooms[socket.room], {
-      id: socket.connId
+        var toID = _.findWhere(process.rooms[socket.room], {
+            id: socket.connId
+        });
+        var toSocket = _.findWhere(process.rooms[toRoom], {
+            id: toID.connect
+        });
+        console.log('toroom', toRoom, toID.connect);
+        if (toID.connect) {
+            toSocket.socket.emit('data', data);
+        }
+
     });
-    var toSocket = _.findWhere(process.rooms[toRoom], {
-      id: toID.connect
+    socketInfo.disconnect(function disconnet() {
+
+        var id = this.connId;
+        var toId = null;
+        var toRoom = (this.room === 'server') ? 'browser' : 'server';
+
+        process.rooms[this.room] = process.rooms[this.room].filter(function(obj) {
+            if (obj.id !== id) {
+                return true;
+            } else {
+                toId = obj.connect;
+                return false;
+            }
+        });
+
+        console.log('connected:', process.rooms.server.length, process.rooms.browser.length);
     });
-    console.log('toroom', toRoom, toID.connect);
-    if (toID.connect) {
-      toSocket.socket.emit('data', data);
-    }
-
-  });
-  socketInfo.disconnect(function disconnet() {
-
-    var id = this.connId;
-    var toId = null;
-    var toRoom = (this.room === 'server') ? 'browser' : 'server';
-
-    process.rooms[this.room] = process.rooms[this.room].filter(function(obj) {
-      if (obj.id !== id) {
-        return true;
-      } else {
-        toId = obj.connect;
-        return false;
-      }
-    });
-
-    console.log('connected:', process.rooms.server.length, process.rooms.browser.length);
-  });
 
 });
 
 class socketsInfo {
-  constructor(socket) {
-    this.socket = socket;
-  }
-  onData(callback) {
-    console.log('waiting', this.socket.connId);
-    var socket = this.socket;
-    this.socket.on('data', function(data) {
+    constructor(socket) {
+        this.socket = socket;
+    }
+    onData(callback) {
+        console.log('waiting', this.socket.connId);
+        var socket = this.socket;
+        this.socket.on('data', function(data) {
 
-      callback(data, socket);
-    });
-  }
-  sendData(data) {
-    this.socket.emit(data);
-  }
-  disconnect(callback) {
-    this.socket.on('disconnect', callback);
-  }
+            callback(data, socket);
+        });
+    }
+    sendData(data) {
+        this.socket.emit(data);
+    }
+    disconnect(callback) {
+        this.socket.on('disconnect', callback);
+    }
 }
